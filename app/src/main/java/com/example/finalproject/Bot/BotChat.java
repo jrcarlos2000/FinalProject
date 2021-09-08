da22ca024ee15210647cf8c52ae6f19a33a814ef
Kevin <kevinpliang@gmail.com>
2021-09-08 00:12:24 -0400

    Modals and icons

---
+++ app/src/main/java/com/example/finalproject/Bot/BotChat.java
@@ -0,0 +1,27 @@
+package com.example.finalproject.Bot;
+
+public class BotChat {
+    private String message;
+    private String sender;
+
+    public BotChat(String message, String sender) {
+        this.message = message;
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/Bot/BotMessage.java
@@ -0,0 +1,18 @@
+package com.example.finalproject.Bot;
+
+public class BotMessage {
+    private String msg;
+
+    // Constructor
+    public BotMessage(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
